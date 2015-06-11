d17fb3f4f12ab5f8b98c18a97768833feb95f8d0
ved62 <v.dembskiy@gmail.com>
2015-06-11 13:51:44 +0300

    Initial Commit

---
+++ Image As Wallpaper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Image As Wallpaper
+//
+//  Created by Владислав Эдуардович Дембский on 11.06.15.
+//  Copyright (c) 2015 Vladislav Dembskiy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
