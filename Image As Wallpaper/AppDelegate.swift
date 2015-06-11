4c21963226edc877d938ced10cbb192750db1929
ved62 <v.dembskiy@gmail.com>
2015-06-11 18:23:55 +0300

    Empty ContentViewController added

--- Image As Wallpaper/AppDelegate.swift
+++ Image As Wallpaper/AppDelegate.swift
@@ -13,15 +13,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
+    @IBOutlet weak var contentViewController: ContentViewController!
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
 
 }
 
